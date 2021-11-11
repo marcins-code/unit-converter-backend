660d91f2aa4aee6d3067cb9b895f68ca67226c1b
marcin's-code <mpaczkowski40@gmail.com>
2021-11-11 23:45:59 +0100

    Change config

--- jest.config.ts
+++ jest.config.ts
@@ -4,9 +4,9 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest'
   },
   // testRegex: '(/__tests__/.*|(\\\\.|/)(test|spec))\\\\.[jt]sx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
-  // collectCoverage: true,
-//   collectCoverageFrom: [
-//     "src/**/*.ts"
-//   ]
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts'
+  ]
 };
