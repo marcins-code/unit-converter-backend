74b69b8d0f7d205bfc9d661abd03c471c27ca7f9
marcin's-code <mpaczkowski40@gmail.com>
2021-11-27 00:53:56 +0100

    Split app and server

--- dist/server.js
+++ dist/server.js
@@ -6,5 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var app_1 = __importDefault(require("./app"));
 var port = 3000;
 // Start server
-app_1.default.listen(port, function () { return console.log("Server is listening on port i dupa " + port + "!"); });
+app_1.default.listen(port, function () {
+    return console.log("Server is listening on port i dupa " + port + "!");
+});
 //# sourceMappingURL=server.js.map
